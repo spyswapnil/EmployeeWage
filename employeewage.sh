1412bb4b3ce31120003e0aae69ebc7ad73d72ca8
Swapnil Sharma <sharma.swapnil245@gmail.com>
2020-08-29 17:52:39 +0530

    Code refactored from if else to Case Statement

--- employeewage.sh
+++ employeewage.sh
@@ -1,2 +1,22 @@
 #! /bin/bash
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+wageperhour=20
+fullday=1
+parttime=2
+attendance=$(( RANDOM%3 ))
+case $attendance in
+	0)
+		echo "Absent"
+		;;
+	1)
+		echo "Full Day"
+		totalhoursperday=8
+		;;
+	2)
+		echo "Half Day"
+		totalhoursperday=4
+		;;
+esac
+
+dailywage=$(( wageperhour*totalhoursperday ))
+echo "Daily Wage:$dailywage"
