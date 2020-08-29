f2e2d4804b635713a01d814235a8cdfd3bc35ea5
Swapnil Sharma <sharma.swapnil245@gmail.com>
2020-08-29 17:37:23 +0530

    Daily Wage added

--- employeewage.sh
+++ employeewage.sh
@@ -1,2 +1,16 @@
 #! /bin/bash
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+
+attendance=$(( RANDOM%2 ))
+wageperhour=20
+totalhoursperday=8
+
+if [ $attendance -eq 0 ]
+then
+	echo "Absent"
+else
+	echo "Present"
+	dailywage=$(( wageperhour*totalhoursperday ))
+	echo "Daily wage is:$dailywage"
+fi
+
