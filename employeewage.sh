8cc8a234f8121d5692f9b1ffde7e1420abfe3439
Swapnil Sharma <sharma.swapnil245@gmail.com>
2020-08-29 18:52:05 +0530

    Total wages calculated

--- employeewage.sh
+++ employeewage.sh
@@ -1,2 +1,29 @@
 #! /bin/bash
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+wageperhour=20
+totalhoursinmonth=0
+totaldaysinmonth=0
+totalwage=0
+function getworkinghours(){
+	case $1 in
+			0)
+				echo "Absent"
+				;;
+			1)
+				totalhoursperday=8
+				;;
+			2)
+				totalhoursperday=4
+				;;
+	esac
+	echo "$totalhoursperday"
+}
+while [ $totalhoursinmonth -le 100 ] && [ $totaldaysinmonth -le 20 ]
+do
+	((totaldaysinmonth++))
+	totalhoursperdays=$( getworkinghours $(( RANDOM%3 )) )
+	totalhoursinmonth=$(( totalhoursinmonth+totalhoursperdays ))
+	dailywage=$(( wageperhour*totalhoursperdays ))
+	totalwage=$(( totalwage+dailywage ))
+done
+echo "Total Wage:$totalwage"
