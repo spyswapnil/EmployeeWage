f29180e7a2ca519b86d53432209e3a3ed48f7f52
Swapnil Sharma <sharma.swapnil245@gmail.com>
2020-08-29 17:58:55 +0530

    No. of days in a month added

--- employeewage.sh
+++ employeewage.sh
@@ -1,2 +1,26 @@
 #! /bin/bash
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+wageperhour=20
+fullday=1
+parttime=2
+totaldaysinmonth=20
+for ((i=1; i<=$totaldaysinmonth; i++))
+do
+	attendance=$(( RANDOM%3 ))
+	case $attendance in
+		0)
+			echo "Absent"
+			;;
+		1)
+			echo "Full Day"
+			totalhoursperday=8
+			;;
+		2)
+			echo "Half Day"
+			totalhoursperday=4
+			;;
+	esac
+
+dailywage=$(( wageperhour*totalhoursperday ))
+echo "Daily Wage:$dailywage"
+done
