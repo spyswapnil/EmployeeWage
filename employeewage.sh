be270d313e9b7e17735d82d2b2ee78b14cd611ba
Swapnil Sharma <sharma.swapnil245@gmail.com>
2020-08-29 18:48:56 +0530

    Function for working hours added

--- employeewage.sh
+++ employeewage.sh
@@ -1,2 +1,28 @@
 #! /bin/bash
+
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+wageperhour=20
+totalhoursinmonth=0
+totaldaysinmonth=0
+function getworkinghours(){
+	case $1 in
+			0)
+				echo "Absent"
+				;;
+			1)
+				totalhoursperday=8
+				;;
+			2)
+				totalhoursperday=4
+				;;
+	esac
+	echo "$totalhoursperday"
+}
+while [ $totalhoursinmonth -le 100 ] && [ $totaldaysinmonth -le 20 ]
+do
+	((totaldaysinmonth++))
+	totalhoursperdays=$( getworkinghours $(( RANDOM%3 )) )
+	totalhoursinmonth=$(( totalhoursinmonth+totalhoursperdays ))
+	dailywage=$(( wageperhour*totalhoursperdays ))
+	echo "Daily Wage:$dailywage"
+done
