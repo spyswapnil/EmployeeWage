c1eef38e85e05ee26200f209baa1d6177fb82147
Swapnil Sharma <sharma.swapnil245@gmail.com>
2020-08-29 17:46:16 +0530

    Part Time Employee added

--- employeewage.sh
+++ employeewage.sh
@@ -1,2 +1,20 @@
 #! /bin/bash
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+wageperhour=20
+fullday=1
+parttime=2
+attendance=$(( RANDOM%3 ))
+if [ $attendance -eq 0 ]
+then
+	echo "Absent"
+elif [ $attendance -eq 1 ]
+then
+	echo "Full Day"
+	totalhoursperday=8
+else
+	echo "Half Day"
+	totalhoursperday=4
+fi
+
+dailywage=$(( wageperhour*totalhoursperday ))
+echo "Daily Wage:$dailywage"
