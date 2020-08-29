3c7f04aec35afa4f38430045093212210e3ce029
Swapnil Sharma <sharma.swapnil245@gmail.com>
2020-08-29 17:29:19 +0530

    Attendance added

--- employeewage.sh
+++ employeewage.sh
@@ -1,2 +1,11 @@
 #! /bin/bash
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+
+attendance=$(( RANDOM%2 ))
+
+if [ $attendance -eq 0 ]
+then
+	echo "Absent"
+else
+	echo "Present"
+fi
